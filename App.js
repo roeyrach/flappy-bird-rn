75f9ecb4471dab8d08adb90be46f4aaec1e7d104
roeyrach <93585090+roeyrach@users.noreply.github.com>
2023-01-13 14:03:27 +0200

    testing branch

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native"
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Text>Roey's hi</Text>
+			<Text>Roey's sdjgh</Text>
 			<StatusBar style="auto" />
 		</View>
 	)
